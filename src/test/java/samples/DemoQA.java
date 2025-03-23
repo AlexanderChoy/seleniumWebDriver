ab6f913f06c28149bf0173ea7ab2f5fcd19f8db6
AlexanderChoy <sllevin.jr@gmail.com>
2025-03-23 03:02:30 +0300

    DemoQA first try

---
+++ src/test/java/samples/DemoQA.java
@@ -0,0 +1,101 @@
+package samples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DemoQA {
+
+    WebDriver dr = null;
+    String browser = System.getProperty("browser");
+
+    public static WebElement waitForElement(WebDriver dr, WebDriverWait wait, By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    @Test
+    public void openDemoQATest() throws InterruptedException {
+
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            dr = new ChromeDriver();
+        } else if (browser.equals("safari")) {
+            WebDriverManager.safaridriver().setup();
+            dr = new SafariDriver();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            dr = new FirefoxDriver();
+        }
+
+        String url = "https://demoqa.com/automation-practice-form";
+
+        dr.get(url);
+        dr.manage().window().maximize();
+
+        dr.findElement(By.id("firstName")).sendKeys("Alex");
+        dr.findElement(By.id("lastName")).sendKeys("Choy");
+        dr.findElement(By.id("userEmail")).sendKeys("alex@gmail.com");
+        dr.findElement(By.xpath("//label[contains(text(), 'Male')]")).click();
+        dr.findElement(By.id("userNumber")).sendKeys("9999999999");
+        dr.findElement(By.id("dateOfBirthInput")).sendKeys("20 Dec 2000");
+
+        dr.findElement(By.xpath("//label[contains(text(), 'Sports')]")).click();
+        dr.findElement(By.xpath("//label[contains(text(), 'Music')]")).click();
+        dr.findElement(By.id("currentAddress")).sendKeys("Moscow, Lomonosov street 5-25");
+
+        WebDriverWait wait = new WebDriverWait(dr, Duration.ofSeconds(2));
+
+        WebElement stateDropdown = waitForElement(dr, wait, By.id("state"));
+        stateDropdown.click();
+
+        WebElement stateOption = waitForElement(dr, wait, By.xpath("//div[text()='Uttar Pradesh']"));
+        stateOption.click();
+
+        // Используем метод для ожидания города
+        WebElement cityDropdown = waitForElement(dr, wait, By.id("city"));
+        cityDropdown.click();
+
+        // Пример выбора города
+        WebElement cityOption = waitForElement(dr, wait, By.xpath("//div[text()='Agra']"));
+        cityOption.click();
+
+        dr.findElement(By.id("submit")).click();
+
+        waitForElement(dr, wait, By.className("modal-content"));
+
+        String name = waitForElement(dr, wait, By.xpath("//td[text()='Student Name']/following-sibling::td")).getText();
+        String email = waitForElement(dr, wait, By.xpath("//td[text()='Student Email']/following-sibling::td")).getText();
+        String gender = waitForElement(dr, wait, By.xpath("//td[text()='Gender']/following-sibling::td")).getText();
+        String phone = waitForElement(dr, wait, By.xpath("//td[text()='Mobile']/following-sibling::td")).getText();
+        String address = waitForElement(dr, wait, By.xpath("//td[text()='Address']/following-sibling::td")).getText();
+        String stateCity = waitForElement(dr, wait, By.xpath("//td[text()='State and City']/following-sibling::td")).getText();
+
+        assert name.equals("Alex Choy") : "Name or second name do not match!";
+        assert email.equals("alex@gmail.com") : "Email does not match!";
+        assert gender.equals("Male") : "Gender does not match!";
+        assert phone.equals("9999999999") : "Phone number does not match!";
+        assert address.equals("Moscow, Lomonosov street 5-25") : "Address does not match!";
+        assert stateCity.equals("Uttar Pradesh Agra") : "State or City do not match!";
+
+        dr.quit();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (dr != null) {
+            dr.quit();
+        }
+    }
+
+}
+
